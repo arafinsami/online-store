2d59f782ed0a1672fe6748ee70cb96c4b8cbfc2d
arafin <sami.cse.1112@gmail.com>
2023-02-22 00:38:18 +0800

    thymeleaf comfig added

--- src/main/java/com/online/config/MvcConfig.java
+++ src/main/java/com/online/config/MvcConfig.java
@@ -1,15 +1,17 @@
 package com.online.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -17,17 +19,31 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan(basePackages = { "${scan.controller}" })
 public class MvcConfig implements WebMvcConfigurer {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
-		return viewResolver;
+	public ViewResolver thymeleafResolver() {
+		ThymeleafViewResolver ivr = new ThymeleafViewResolver();
+		ivr.setTemplateEngine(templateEngine());
+		ivr.setOrder(0);
+		return ivr;
 	}
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver srtr = new SpringResourceTemplateResolver();
+		srtr.setApplicationContext(applicationContext);
+		srtr.setPrefix("/WEB-INF/views/");
+		srtr.setSuffix(".html");
+		return srtr;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
 	}
 }
