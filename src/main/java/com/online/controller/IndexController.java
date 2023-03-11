503477845a01c0bff7113ae299cd20eee861f404
arafin <sami.cse.1112@gmail.com>
2023-03-11 19:12:54 +0800

    index controller updated

--- src/main/java/com/online/controller/IndexController.java
+++ src/main/java/com/online/controller/IndexController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.online.entity.Product;
 import com.online.model.ProductModel;
 import com.online.service.ProductService;
 
